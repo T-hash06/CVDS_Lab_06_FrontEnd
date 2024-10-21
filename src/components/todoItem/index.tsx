de888a6b3de54a6bf28a2c88c00856b415112e0f
t-hash06 <tomasfelipe.p@gmail.com>
2024-10-20 21:10:01 -0500

    feat: add chip colors to difficulty and priority on TodoItem component

--- src/components/todoItem/index.tsx
+++ src/components/todoItem/index.tsx
@@ -18,6 +18,80 @@ type Todo = {
 
 type TodoItemProps = React.LiHTMLAttributes<HTMLLIElement> & Todo;
 
+interface DifficultyChipProps extends React.HTMLAttributes<HTMLSpanElement> {
+	difficulty: TodoDifficulty;
+}
+
+interface PriorityChipProps extends React.HTMLAttributes<HTMLSpanElement> {
+	priority: TodoPriority;
+}
+
+function DifficultyChip(props: DifficultyChipProps) {
+	const { difficulty, ...restProps } = props;
+
+	const chipStyles = {
+		low: {
+			color: 'hsl(130, 100%, 19%)',
+			backgroundColor: 'hsl(130, 70%, 60%)',
+		},
+		medium: {
+			color: 'hsl(48, 100%, 19%)',
+			backgroundColor: 'hsl(48, 100%, 50%)',
+		},
+		high: {
+			color: 'hsl(350, 100%, 19%)',
+			backgroundColor: 'hsl(350, 90%, 60%)',
+		},
+	};
+
+	return (
+		<span
+			{...restProps}
+			className={`${styles.difficulty} ${styles[difficulty]}`}
+			style={{ ...chipStyles[difficulty] }}
+		>
+			{difficulty}
+		</span>
+	);
+}
+
+function PriorityChip(props: PriorityChipProps) {
+	const { priority, ...restProps } = props;
+
+	const chipStyles = {
+		1: {
+			color: 'hsl(130, 100%, 19%)',
+			backgroundColor: 'hsl(130, 70%, 60%)',
+		},
+		2: {
+			color: 'hsl(130, 100%, 19%)',
+			backgroundColor: 'hsl(130, 70%, 60%)',
+		},
+		3: {
+			color: 'hsl(48, 100%, 19%)',
+			backgroundColor: 'hsl(48, 100%, 50%)',
+		},
+		4: {
+			color: 'hsl(48, 100%, 19%)',
+			backgroundColor: 'hsl(48, 100%, 50%)',
+		},
+		5: {
+			color: 'hsl(350, 100%, 19%)',
+			backgroundColor: 'hsl(350, 90%, 60%)',
+		},
+	} as const;
+
+	return (
+		<span
+			{...restProps}
+			className={`${styles.priority} ${styles[`priority${priority}`]}`}
+			style={{ ...chipStyles[priority] }}
+		>
+			P{priority}
+		</span>
+	);
+}
+
 export function TodoItem(props: TodoItemProps) {
 	const { id, name, description, difficulty, priority, done, ...restProps } =
 		props;
@@ -26,8 +100,8 @@ export function TodoItem(props: TodoItemProps) {
 		<li {...restProps} className={`${styles.todoItem}`}>
 			<h2 className={styles.name}>{name}</h2>
 			<p className={styles.description}>{description}</p>
-			<span className={styles.difficulty}>{difficulty}</span>
-			<span className={styles.priority}>P{priority}</span>
+			<DifficultyChip difficulty={difficulty} />
+			<PriorityChip priority={priority} />
 			<button
 				className={`${styles.button} ${styles.doneButton}`}
 				type='button'
